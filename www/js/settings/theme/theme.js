4fdb3a1904780bd81500ab43847115c135a0a189
Andre Eife <andre.eife@gmail.com>
2015-02-15 04:22:29 +0100

    theme active status

--- www/js/settings/theme/theme.js
+++ www/js/settings/theme/theme.js
@@ -2,6 +2,9 @@ angular.module('clicker.settings.theme', [])
     .controller('ThemeCtrl', function ($scope, settingsData) {
         $scope.ThemeCtrl = {
             themes: settingsData.themes,
+            getActiveTheme: function () {
+                return settingsData.activeTheme;
+            },
             setActiveTheme: function (theme) {
                 settingsData.change.theme(theme);
             }
