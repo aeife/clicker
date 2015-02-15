359aff463dcefd87b2bcb4a828e8ac3104ddbe68
Andre Eife <andre.eife@gmail.com>
2015-02-15 15:45:29 +0100

    json email export

--- www/js/export/export.js
+++ www/js/export/export.js
@@ -1,8 +1,27 @@
-angular.module('clicker.export', [])
-    .controller('ExportCtrl', function ($scope) {
+angular.module('clicker.export', ['ngCordova', 'components.clickData'])
+    .controller('ExportCtrl', function ($scope, $cordovaEmailComposer, clickData, $cordovaFile) {
+        var generateJSON = function (data) {
+            return 'base64:data.json//' + btoa(JSON.stringify(data));
+        };
+
         $scope.ExportCtrl = {
             email: function () {
+                console.log(generateJSON(clickData.getClicks()));
+                $cordovaEmailComposer.isAvailable().then(function() {
+                    var email = {
+                        //to: 'max@mustermann.de',
+                        //cc: 'erika@mustermann.de',
+                        //bcc: ['john@doe.com', 'jane@doe.com'],
+                        attachments: generateJSON(clickData.getClicks()),
+                        subject: 'Clicker Export',
+                        body: 'Exported data from Clicker.',
+                        isHtml: true
+                    };
 
+                    $cordovaEmailComposer.open(email).then(null, function () {
+                        // user cancelled email
+                    });
+                });
             }
         };
     });
