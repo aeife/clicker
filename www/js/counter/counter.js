9933ac878e7c8ceca5d8bd7ea4cfa2f485723ab7
Andre Eife <andre.eife@gmail.com>
2015-02-21 00:41:08 +0100

    use cordova media instead of html5 audio

--- www/js/counter/counter.js
+++ www/js/counter/counter.js
@@ -1,20 +1,19 @@
 angular.module('clicker.counter', ['components.stopParentClick', 'components.clickData', 'components.settingsData'])
-    .controller('CounterCtrl', function ($scope, clickData, settingsData, $timeout, $ionicModal) {
-        // initialize sound several times so it can be played fast in sequence
-        // pre initialization for performance reasons
-        var sounds = [];
-        _.times(10, function () {
-            sounds.push(new Audio("audio/beep.wav"));
-        });
-        var soundCounter = 0;
+    .controller('CounterCtrl', function ($scope, clickData, settingsData, $timeout, $ionicModal, $ionicPlatform) {
+        var sound;
+
+        var instantiateSound = function () {
+            $ionicPlatform.ready(function () {
+                sound = new Media('audio/beep.wav');
+            });
+        };
 
         var playSound = function () {
             if (settingsData.sound) {
-                sounds[soundCounter].play();
-                soundCounter++;
-                if (soundCounter > 9) {
-                    soundCounter = 0;
+                if (!sound) {
+                    instantiateSound();
                 }
+                sound.play();
             }
         };
 
